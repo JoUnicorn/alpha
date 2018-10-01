9623deabfb706ffec19ea0f237ca66996f863204
JoUnicorn <jogithub@gmail.com>
2018-10-01 22:20:21 +0800

    init visualization page

--- visualization/views.py
+++ visualization/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
+    return HttpResponse("""
+        <h1>Data visualization !</h1>
+    """)
