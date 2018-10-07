781dcab12f2660be600a8f5dfae227b8cfae6fea
JoUnicorn <jogithub@gmail.com>
2018-10-07 15:57:15 +0800

    search button + table

--- visualization/urls.py
+++ visualization/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('stock', views.stock),
+    re_path(r'^search_s/$', views.search_s, name='search_s'),
+    re_path(r'^return_s_data/$', views.return_s_data, name='return_s_data'),
 ]
--- visualization/views.py
+++ visualization/views.py
@@ -2,16 +2,33 @@ from datetime import datetime
 from django.shortcuts import render
 import requests
 import json
+from django.http import JsonResponse
 
 API_URL = "https://www.alphavantage.co/query"
 
-data = {
-    "function": "TIME_SERIES_DAILY",
-    "symbol": "MSFT",
-    "apikey": "Q5I0WW1DQE5CBEQH",
-}
-
-response = requests.get(API_URL, params=data).json()
-
 def stock(request):
-    return render(request, 'visualization/stock.html', {'date': datetime.now(), 'stocks': response['Time Series (Daily)']})
+    return render(request, 'visualization/stock.html', {'date': datetime.now()})
+
+def search_s(request):
+    symbol = request.GET['symbol']
+    data = {
+        "function": "SYMBOL_SEARCH",
+        "keywords": symbol,
+        "apikey": "Q5I0WW1DQE5CBEQH",
+    }
+
+    response = requests.get(API_URL, params=data).json()
+
+    return JsonResponse(response)
+
+def return_s_data(request):
+    symbol = request.GET['symbol']
+    data = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": symbol,
+        "apikey": "Q5I0WW1DQE5CBEQH",
+    }
+
+    response = requests.get(API_URL, params=data).json()
+
+    return JsonResponse(response['Time Series (Daily)'])
