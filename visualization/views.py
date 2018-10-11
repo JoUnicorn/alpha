746f9374adde0ef732eb1fa076e283a57e84953e
JoUnicorn <jogithub@gmail.com>
2018-10-11 22:05:21 +0800

    get data

--- visualization/views.py
+++ visualization/views.py
@@ -4,6 +4,7 @@ import requests
 import json
 from django.http import JsonResponse
 from .models import Category_sector, Category_industry, Stock
+from django.core import serializers
 
 API_URL = "https://www.alphavantage.co/query"
 
@@ -12,16 +13,27 @@ def stock(request):
 
 def search_s(request):
     symbol = request.GET['symbol']
-    data = {
-        "function": "SYMBOL_SEARCH",
-        "keywords": symbol,
-        "apikey": "Q5I0WW1DQE5CBEQH",
+
+#    data = {
+#        "function": "SYMBOL_SEARCH",
+#        "keywords": symbol,
+#        "apikey": "Q5I0WW1DQE5CBEQH",
+#    }
+
+#    response = requests.get(API_URL, params=data).json()
+
+    response2 =Stock.objects.filter(symbol__startswith=symbol.upper()).order_by('symbol')
+    stack=[]
+    for data in response2:
+        stack.append({'1. symbol':data.symbol, '2. name':data.company, 'sector':data.category_sector.sector, 'industry':data.category_industry.industry, 'exchange':data.exchange, 'ipo':data.ipoYear})
+
+    data={
+        'data':stack
     }
 
-    response = requests.get(API_URL, params=data).json()
-    print(get_object_or_404(Stock, id=5))
+    print(data)
 
-    return JsonResponse(response)
+    return JsonResponse(data)
 
 def return_s_data(request):
     symbol = request.GET['symbol']
